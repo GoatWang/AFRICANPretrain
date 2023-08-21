f1bfb1ab399ffebd4f48bb524a22ab2b7e8fa909
GoatWang <jeremy4555@yahoo.com.tw>
2023-08-21 14:59:21 +0100

    plotting

--- Train/Dataset.py
+++ Train/Dataset.py
@@ -89,9 +89,9 @@ class AnimalKingdomDatasetVisualize(AnimalKingdomDataset):
         elif self.mode == "simmat":
             video_frames_raw_out = (video_frames_raw.detach().cpu().numpy().transpose(0, 2, 3, 1) * 255).astype("uint8")
             video_frames1 = self.video_aug(video_frames_raw, self.video_transform).detach().cpu().numpy()
-            video_frames1 = (video_frames.transpose(0, 2, 3, 1) * 255).astype("uint8")
+            video_frames1 = (video_frames1.transpose(0, 2, 3, 1) * 255).astype("uint8")
             video_frames2 = self.video_aug(video_frames_raw, self.video_transform).detach().cpu().numpy()
-            video_frames2 = (video_frames.transpose(0, 2, 3, 1) * 255).astype("uint8")
+            video_frames2 = (video_frames2.transpose(0, 2, 3, 1) * 255).astype("uint8")
             return video_fp, video_frames_raw_out, video_frames1, video_frames2
     
     def __len__(self):
